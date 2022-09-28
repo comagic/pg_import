743a6e77038ec9af47d19663b8810d0e429e7d32
Andrey Chernyakov <a.chernyakov@comagic.dev>
2022-09-28 21:40:34 +0300

    ci/cd

--- setup.py
+++ setup.py
@@ -46,13 +46,12 @@ install_requires = [
 
 # Что нужно для запуска python setup.py test
 tests_require = [
-    'pytest',
-    'pytest-cov',
-    'pytest-flake8',
-    'pytest-asyncio',
-    'pytest-sugar',
-    'asynctest',
-    'flake8<4'
+    'flake8==3.8.3',
+    'pytest==5.4.3',
+    'pytest-cov==2.9.0',
+    'pytest-flake8==1.0.6',
+    'pytest-asyncio==0.12.0',
+    'asynctest==0.13.0',
 ]
 
 
