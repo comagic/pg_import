8f7580496e18e2cc0833655fe61cd8d69801ee7d
Andrey Chernyakov <a.chernyakov@comagic.dev>
2025-10-17 19:25:00 +0300

    actualize dockerfile

--- docker-image/cmd.sh
+++ docker-image/cmd.sh
@@ -3,16 +3,31 @@
 # env variable required:
 # DB_NAME
 # DB_SRC_DIR
-# PG_USER
-# PG_PASSWORD
-# PG_HOST
-# PG_PORT
+# PGUSER
+# PGPASSWORD
+# PGHOST
+# PGPORT
+# ROLES_FILE
+# AFTER_BUILD_SCRIPT
+# TIMEOUT
 
+if [ -z $TIMEOUT ]; then
+  TIMEOUT=1
+fi
 
+sleep $TIMEOUT
 echo start building database $DB_NAME from $DB_SRC_DIR
-pg_import --rebuild -r $ROLES -d $DB_NAME $DB_SRC_DIR
+if [ -z $ROLES_FILE ]; then
+  pg_import --rebuild -d $DB_NAME $DB_SRC_DIR
+else
+  pg_import --rebuild -r $ROLES_FILE -d $DB_NAME $DB_SRC_DIR
+fi
+
+if [[ ! -z $AFTER_BUILD_SCRIPT ]]; then
+  psql -c "$AFTER_BUILD_SCRIPT" -d $DB_NAME
+fi
 echo done
 
 if [ "$SIDECAR_MOD" == "true" ]; then
-sleep infinity
+  sleep infinity
 fi
--- docker-image/rebuild.sh
+++ docker-image/rebuild.sh
@@ -1,13 +1,21 @@
 set -o xtrace
 
-docker image rm andruche/pg_import:2.1.0-arm64
-docker image rm andruche/pg_import:2.1.0-amd64
-docker image rm andruche/pg_import:2.1.0
-docker buildx build --platform linux/arm64/v8 -f Dockerfile -t andruche/pg_import:2.1.0-arm64 .
-docker buildx build --platform linux/amd64 -f Dockerfile -t andruche/pg_import:2.1.0-amd64 .
-docker push andruche/pg_import:2.1.0-amd64
-docker push andruche/pg_import:2.1.0-arm64
-docker manifest rm andruche/pg_import:2.1.0
-docker manifest create andruche/pg_import:2.1.0 --amend andruche/pg_import:2.1.0-arm64 --amend andruche/pg_import:2.1.0-amd64
-docker manifest push andruche/pg_import:2.1.0
-docker pull andruche/pg_import:2.1.0
+version=2.5.0
+
+docker image rm andruche/pg_import:$version-arm64
+docker image rm andruche/pg_import:$version-amd64
+docker image rm andruche/pg_import:$version
+docker manifest rm andruche/pg_import:$version
+docker manifest rm andruche/pg_import:latest
+
+set -e
+
+docker buildx build --platform linux/arm64/v8 -f Dockerfile -t andruche/pg_import:$version-arm64 .
+docker buildx build --platform linux/amd64 -f Dockerfile -t andruche/pg_import:$version-amd64 .
+docker push andruche/pg_import:$version-arm64
+docker push andruche/pg_import:$version-amd64
+docker manifest create andruche/pg_import:$version --amend andruche/pg_import:$version-arm64 --amend andruche/pg_import:$version-amd64
+docker manifest push andruche/pg_import:$version
+docker manifest create andruche/pg_import:latest --amend andruche/pg_import:$version-arm64 --amend andruche/pg_import:$version-amd64
+docker manifest push andruche/pg_import:latest
+docker pull andruche/pg_import:$version
